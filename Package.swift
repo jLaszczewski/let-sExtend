686001124e6bc3983b8bf1f855f06cdb00617275
Jakub Łaszczewski <j.laszczewski@itgenerator.com>
2020-11-13 10:32:25 +0100

    Unify names.

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-  name: "letsExtend",
+  name: "let-sExtend",
   platforms: [.iOS(.v14), .macOS(.v11), .watchOS(.v6), .tvOS(.v14)],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
-      name: "letsExtend",
-      targets: ["letsExtend"]),
+      name: "let-sExtend",
+      targets: ["let-sExtend"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
-      name: "letsExtend",
+      name: "let-sExtend",
       dependencies: []),
     .testTarget(
-      name: "letsExtendTests",
-      dependencies: ["letsExtend"]),
+      name: "let-sExtendTests",
+      dependencies: ["let-sExtend"]),
   ]
 )
