35ba8885a0c537dab75763e5cae5407d298fb24e
Eugeny Hil <e.hil@itgenerator.pl>
2021-01-25 14:15:15 +0300

    articles added refreshableScroll + activityIndicator

---
+++ Sources/let-sExtend/SwiftUI/Components/ActivityIndicator.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  
+//
+//  Created by ITgenerator on 25/01/2021.
+//
+
+import SwiftUI
+
+public struct ActivityIndicator: UIViewRepresentable {
+  
+  typealias UIView = UIActivityIndicatorView
+  
+  private var isAnimating = true
+  private var configuration = { (indicator: UIView) in }
+
+  public init(
+    isAnimating: Bool,
+    configuration: ((UIView) -> Void)? = nil
+  ) {
+    self.isAnimating = isAnimating
+    if let configuration = configuration {
+      self.configuration = configuration
+    }
+  }
+
+  public func makeUIView(
+    context: UIViewRepresentableContext<Self>
+  ) -> UIView {
+    UIView()
+  }
+
+  public func updateUIView(
+    _ uiView: UIView,
+    context: UIViewRepresentableContext<Self>
+  ) {
+    isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    configuration(uiView)
+  }
+}
