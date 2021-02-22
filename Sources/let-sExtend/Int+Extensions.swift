d6beb81e097edf0a8aabbedfbacb4ce446bc0566
Eugeny Hil <e.hil@itgenerator.pl>
2021-02-22 14:51:34 +0300

    measurements-hotfixes added extension methods

--- Sources/let-sExtend/Int+Extensions.swift
+++ Sources/let-sExtend/Int+Extensions.swift
@@ -18,4 +18,9 @@ public extension Int {
   ) -> String {
     String(format: "%0\(intPart)d", self)
   }
+  
+  var double: Double {
+    get { Double(self) }
+    set { self = Int(newValue) }
+  } 
 }
